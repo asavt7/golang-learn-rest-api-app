43b845b1fe196e7a80427766dfd8a32c136eef8b
asavt <asavt@example.com>
2021-09-01 18:04:26 +0400

    add postgress

--- cmd/main.go
+++ cmd/main.go
@@ -16,8 +16,20 @@ func main() {
 		log.Fatalf("error while init configs %s", err.Error())
 	}
 
+	db, err := repos.NewPostgreDb(repos.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.name"),
+		SSLMode:  viper.GetString("db.ssl.mode"),
+	})
+	if err != nil {
+		log.Fatalf("error init database %s", err.Error())
+	}
+
 	s := new(todo.Server)
-	repo := repos.NewRepo()
+	repo := repos.NewPostgresRepo(db)
 	service := services.NewService(repo)
 	handler := handlers.NewHandler(service)
 
