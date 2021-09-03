a6848f7c0f5d1d62c45b74afbd9326f92f138eb1
asavt <asavt@example.com>
2021-09-03 11:06:14 +0400

    gracefullyShutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	todo "github.com/asavt7/todo"
 	"github.com/asavt7/todo/pkg/handlers"
 	"github.com/asavt7/todo/pkg/repos"
 	"github.com/asavt7/todo/pkg/services"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -34,11 +38,33 @@ func main() {
 	service := services.NewService(repo)
 	handler := handlers.NewHandler(service)
 
-	err = s.Run(viper.GetString("port"), handler.InitRoutes())
+	go func() {
+		err = s.Run(viper.GetString("port"), handler.InitRoutes())
+		if err != nil {
+			logrus.Fatalf("error while running server %s", err.Error())
+			return
+		}
+	}()
+
+	logrus.Info("App started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Info("Server shutting down")
+
+	err = s.Shutdown(context.Background())
 	if err != nil {
-		logrus.Fatalf("error while running server %s", err.Error())
-		return
+		logrus.Errorf("error occurred while server shutting down : %s", err.Error())
 	}
+	logrus.Info("Server shut down")
+
+	err = db.Close()
+	if err != nil {
+		logrus.Errorf("error occurred while closing db connection : %s", err.Error())
+	}
+	logrus.Info("DB connection closed")
 }
 
 func initConfig() error {
